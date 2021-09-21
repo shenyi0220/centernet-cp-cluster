e6cedc5eaa5bbda0ba33bdce676b47f881e7dad4
Allen Shen <ashen@nvidia.com>
2021-09-22 00:15:12 +0800

    Implement BP Soft NMS

--- src/lib/detectors/ctdet.py
+++ src/lib/detectors/ctdet.py
@@ -11,6 +11,7 @@ import torch
 try:
   from external.nms import soft_nms
   from external.nms import nms
+  from external.nms import soft_bp_nms, soft_bp_nms_v2
 except:
   print('NMS not imported! If you need it,'
         ' do \n cd $CenterNet_ROOT/src/lib/external \n make')
@@ -65,7 +66,8 @@ class CtdetDetector(BaseDetector):
         [detection[j] for detection in detections], axis=0).astype(np.float32)
       if len(self.scales) > 1 or self.opt.nms:
         if self.opt.nms_method == 'soft':
-          soft_nms(results[j], Nt=0.5, method=2, opt_sna=self.opt.nms_opt_sna, sna_threshold=self.opt.nms_sna_threshold, opt_sai=self.opt.nms_opt_sai)
+          #soft_nms(results[j], Nt=0.5, method=1, opt_sna=self.opt.nms_opt_sna, sna_threshold=self.opt.nms_sna_threshold, opt_sai=self.opt.nms_opt_sai)
+          soft_bp_nms_v2(results[j], Nt=0.5, method=1, opt_sna=self.opt.nms_opt_sna, sna_threshold=self.opt.nms_sna_threshold)
         elif self.opt.nms_method == 'normal':
           nms(results[j], thresh=0.5)
     scores = np.hstack(
--- src/lib/external/nms.pyx
+++ src/lib/external/nms.pyx
@@ -210,6 +210,266 @@ def soft_nms(np.ndarray[float, ndim=2] boxes, float sigma=0.5, float Nt=0.3, flo
     keep = [i for i in range(N)]
     return keep
 
+def swap_array_val(np.ndarray[float, ndim=2] arrs, unsigned int idx1, unsigned int idx2):
+    cdef unsigned int D = arrs.shape[1]
+    cdef float tmpVal
+    if idx1 == idx2:
+        return
+    for i in range(D):
+        tmpVal = arrs[idx1, i]
+        arrs[idx1, i] = arrs[idx2, i]
+        arrs[idx2, i] = tmpVal
+
+def soft_bp_nms(np.ndarray[float, ndim=2] boxes, float sigma=0.5, float Nt=0.3, float threshold=0.001, unsigned int method=0,
+             int opt_sna=0, float sna_threshold=0.8, int opt_sai=0):
+    cdef unsigned int N = boxes.shape[0]
+    cdef float iw, ih, box_area
+    cdef float ua
+    cdef int pos = 0
+    cdef int maxiter = 2
+    cdef float x1,x2,y1,y2,tx1,tx2,ty1,ty2,ts,area,weight,ov,auxProposalNumber,auxMaxConf, auxMaxX1, auxMaxX2, auxMaxY1, auxMaxY2
+    cdef np.ndarray[np.float32_t, ndim=2] posTerms = np.zeros([N, 6], dtype=np.float32)
+    cdef np.ndarray[np.float32_t, ndim=2] negTerms = np.zeros([N, 3], dtype=np.float32)
+    cdef np.ndarray[np.float32_t, ndim=1] iou_thresholds = np.zeros(10, dtype=np.float32)
+    cdef np.ndarray[np.float32_t, ndim=1] alphas = np.ones(10, dtype=np.float32)
+    cdef np.ndarray[np.float32_t, ndim=1] betas = np.ones(10, dtype=np.float32)
+    # Suppress mat: [i, j]=m indicates that Det[i] suppressed Det[j] for m times.
+    cdef np.ndarray[np.int_t, ndim=2] suppressRecodMat = np.zeros([N, N], dtype=np.int)
+    cdef int maxSuppressTime = 1
+    cdef int suppressIdx
+    iou_thresholds[0] = Nt
+    iou_thresholds[1] = 0.75
+    iou_thresholds[2] = 0.8
+    alphas[0] = 1.0
+    betas[0] = 1.0
+    alphas[1] = 1.0
+    betas[1] = 1.0
+    alphas[2] = 1.0
+    betas[2] = 1.0
+
+    for iter in range(maxiter):
+        posTerms = np.zeros([N, 6], dtype=np.float32)
+        negTerms = np.zeros([N, 6], dtype=np.float32)
+        for i in range(N):
+            tx1 = boxes[i,0]
+            ty1 = boxes[i,1]
+            tx2 = boxes[i,2]
+            ty2 = boxes[i,3]
+            ts = boxes[i,4]
+            if ts <= threshold:
+                continue
+
+            # apply sai
+            if opt_sai:
+                Nt = getSizeAwareIOUThresh(tx2 - tx1, ty2 - ty1, 30.0, 100.0, 0.45, 0.6)
+                sna_threshold = getSizeAwareIOUThresh(tx2 - tx1, ty2 - ty1, 50.0, 80.0, 0.8, 0.9)
+
+            # vars for sna
+            auxMaxConf = 0.0
+            auxProposalNumber = 0.0
+            auxMaxX1 = tx1
+            auxMaxX2 = tx2
+            auxMaxY1 = ty1
+            auxMaxY2 = ty2
+
+            pos = 0
+            # NMS iterations, note that N changes if detection boxes fall below threshold
+            for pos in range(N):
+                x1 = boxes[pos, 0]
+                y1 = boxes[pos, 1]
+                x2 = boxes[pos, 2]
+                y2 = boxes[pos, 3]
+                s = boxes[pos, 4]
+
+                if pos == i or s <= threshold or s > ts or suppressRecodMat[i, pos] >= maxSuppressTime:
+                    continue
+
+                area = (x2 - x1 + 1) * (y2 - y1 + 1)
+                iw = (min(tx2, x2) - max(tx1, x1) + 1)
+                if iw > 0:
+                    ih = (min(ty2, y2) - max(ty1, y1) + 1)
+                    if ih > 0:
+                        ua = float((tx2 - tx1 + 1) * (ty2 - ty1 + 1) + area - iw * ih)
+                        ov = iw * ih / ua #iou between max box and detection box
+
+                        if ov > iou_thresholds[iter]:
+                            #if ov > negTerms[pos, 0]:
+                            #    negTerms[pos, 0] = ov
+                            #    negTerms[pos, 2] = (float)(i)
+                            if ts > negTerms[pos, 1]:
+                                negTerms[pos, 0] = ov
+                                negTerms[pos, 1] = ts
+                                negTerms[pos, 2] = (float)(i)
+
+                        if ov >= sna_threshold:
+                            auxProposalNumber = auxProposalNumber + 1.0
+                            if boxes[pos, 4] > auxMaxConf:
+                                auxMaxConf = boxes[pos, 4]
+                                auxMaxX1 = boxes[pos, 0]
+                                auxMaxY1 = boxes[pos, 1]
+                                auxMaxX2 = boxes[pos, 2]
+                                auxMaxY2 = boxes[pos, 3]
+
+            if opt_sna == 1:
+                posTerms[i, 0] = auxProposalNumber
+                posTerms[i, 1] = auxMaxConf
+                posTerms[i, 2] = auxMaxX1
+                posTerms[i, 3] = auxMaxY1
+                posTerms[i, 4] = auxMaxX2
+                posTerms[i, 5] = auxMaxY2
+
+        for i in range(N):
+            if boxes[i, 4] <= threshold:
+                continue
+            boxes[i, 4] = min(1.0, boxes[i, 4] + alphas[iter] * (1.0 - boxes[i,4]) * (posTerms[i, 0] / (posTerms[i, 0] + 1.0)) * posTerms[i, 1])
+            boxes[i, 0] = (boxes[i, 4] * boxes[i, 0] + posTerms[i, 1] * posTerms[i, 2]) / (boxes[i, 4] + posTerms[i, 1])
+            boxes[i, 1] = (boxes[i, 4] * boxes[i, 1] + posTerms[i, 1] * posTerms[i, 3]) / (boxes[i, 4] + posTerms[i, 1])
+            boxes[i, 2] = (boxes[i, 4] * boxes[i, 2] + posTerms[i, 1] * posTerms[i, 4]) / (boxes[i, 4] + posTerms[i, 1])
+            boxes[i, 3] = (boxes[i, 4] * boxes[i, 3] + posTerms[i, 1] * posTerms[i, 5]) / (boxes[i, 4] + posTerms[i, 1])
+            if negTerms[i, 0] > 0.01:
+                boxes[i, 4] = boxes[i, 4] - betas[iter] * boxes[i, 4] * negTerms[i, 0]
+                suppressIdx = (int)(negTerms[i, 2])
+                suppressRecodMat[suppressIdx, i] = suppressRecodMat[suppressIdx, i] + 1
+
+    for i in range(N):
+        if boxes[i, 4] < threshold:
+            swap_array_val(boxes, i, N-1)
+            swap_array_val(negTerms, i, N-1)
+            swap_array_val(posTerms, i, N-1)
+            N = N - 1
+
+    keep = [i for i in range(N)]
+    return keep
+
+def soft_bp_nms_v2(np.ndarray[float, ndim=2] boxes, float sigma=0.5, float Nt=0.3, float threshold=0.01, unsigned int method=0,
+             int opt_sna=0, float sna_threshold=0.8, int opt_sai=0):
+    cdef unsigned int N = boxes.shape[0]
+
+    # Pre-calculate area sizes.
+    cdef np.ndarray[np.float32_t, ndim=1] areas
+
+    cdef float iw, ih, box_area
+    cdef float ua, inter
+    cdef int pos = 0
+    cdef int maxiter = 2
+    cdef float x1,x2,y1,y2,tx1,tx2,ty1,ty2,ts,area,weight,ov,auxProposalNumber,auxMaxConf, auxMaxX1, auxMaxX2, auxMaxY1, auxMaxY2
+    cdef np.ndarray[np.float32_t, ndim=2] posTerms = np.zeros([N, 6], dtype=np.float32)
+    cdef np.ndarray[np.float32_t, ndim=2] negTerms = np.zeros([N, 3], dtype=np.float32)
+    cdef np.ndarray[np.float32_t, ndim=1] iou_thresholds = np.zeros(10, dtype=np.float32)
+    cdef np.ndarray[np.float32_t, ndim=1] alphas = np.ones(10, dtype=np.float32)
+    cdef np.ndarray[np.float32_t, ndim=1] betas = np.ones(10, dtype=np.float32)
+    # Suppress mat: [i, j]=m indicates that Det[i] suppressed Det[j] for m times.
+    cdef np.ndarray[np.int_t, ndim=2] suppressRecodMat = np.zeros([N, N], dtype=np.int)
+    cdef int maxSuppressTime = 1
+    cdef int suppressIdx
+    iou_thresholds[0] = Nt
+    iou_thresholds[1] = 0.75
+    iou_thresholds[2] = 0.8
+    alphas[0] = 1.0
+    betas[0] = 1.0
+    alphas[1] = 1.0
+    betas[1] = 1.0
+    alphas[2] = 1.0
+    betas[2] = 1.0
+
+    for iter in range(maxiter):
+        posTerms = np.zeros([N, 6], dtype=np.float32)
+        negTerms = np.zeros([N, 6], dtype=np.float32)
+        areas = (boxes[:, 2] - boxes[:, 0] + 1) * (boxes[:, 3] - boxes[:, 1] + 1)
+        for i in range(N):
+            tx1 = boxes[i,0]
+            ty1 = boxes[i,1]
+            tx2 = boxes[i,2]
+            ty2 = boxes[i,3]
+            ts = boxes[i,4]
+            tarea = areas[i]
+            if ts <= threshold:
+                continue
+
+            # apply sai
+            if opt_sai:
+                Nt = getSizeAwareIOUThresh(tx2 - tx1, ty2 - ty1, 30.0, 100.0, 0.45, 0.6)
+                sna_threshold = getSizeAwareIOUThresh(tx2 - tx1, ty2 - ty1, 50.0, 80.0, 0.8, 0.9)
+
+            # vars for sna
+            auxMaxConf = 0.0
+            auxProposalNumber = 0.0
+            auxMaxX1 = tx1
+            auxMaxX2 = tx2
+            auxMaxY1 = ty1
+            auxMaxY2 = ty2
+
+            pos = 0
+            # NMS iterations, note that N changes if detection boxes fall below threshold
+            for pos in range(N):
+                x1 = boxes[pos, 0]
+                y1 = boxes[pos, 1]
+                x2 = boxes[pos, 2]
+                y2 = boxes[pos, 3]
+                s = boxes[pos, 4]
+                area = areas[pos]
+
+                if pos == i or s <= threshold or s > ts or suppressRecodMat[i, pos] >= maxSuppressTime:
+                    continue
+
+                # area = (x2 - x1 + 1) * (y2 - y1 + 1)
+                iw = (min(tx2, x2) - max(tx1, x1) + 1)
+                if iw > 0:
+                    ih = (min(ty2, y2) - max(ty1, y1) + 1)
+                    if ih > 0:
+                        inter = iw * ih
+                        ua = float(tarea + area - inter)
+                        ov = inter / ua #iou between max box and detection box
+
+                        if ov > iou_thresholds[iter]:
+                            #if ov > negTerms[pos, 0]:
+                            #    negTerms[pos, 0] = ov
+                            #    negTerms[pos, 2] = (float)(i)
+                            if ts > negTerms[pos, 1]:
+                                negTerms[pos, 0] = ov
+                                negTerms[pos, 1] = ts
+                                negTerms[pos, 2] = (float)(i)
+
+                        if ov >= sna_threshold:
+                            auxProposalNumber = auxProposalNumber + 1.0
+                            if boxes[pos, 4] > auxMaxConf:
+                                auxMaxConf = boxes[pos, 4]
+                                auxMaxX1 = boxes[pos, 0]
+                                auxMaxY1 = boxes[pos, 1]
+                                auxMaxX2 = boxes[pos, 2]
+                                auxMaxY2 = boxes[pos, 3]
+
+            if opt_sna == 1:
+                posTerms[i, 0] = auxProposalNumber
+                posTerms[i, 1] = auxMaxConf
+                posTerms[i, 2] = auxMaxX1
+                posTerms[i, 3] = auxMaxY1
+                posTerms[i, 4] = auxMaxX2
+                posTerms[i, 5] = auxMaxY2
+
+        for i in range(N):
+            if boxes[i, 4] <= threshold:
+                continue
+            boxes[i, 4] = min(1.0, boxes[i, 4] + alphas[iter] * (1.0 - boxes[i,4]) * (posTerms[i, 0] / (posTerms[i, 0] + 1.0)) * posTerms[i, 1])
+            boxes[i, 0] = (boxes[i, 4] * boxes[i, 0] + posTerms[i, 1] * posTerms[i, 2]) / (boxes[i, 4] + posTerms[i, 1])
+            boxes[i, 1] = (boxes[i, 4] * boxes[i, 1] + posTerms[i, 1] * posTerms[i, 3]) / (boxes[i, 4] + posTerms[i, 1])
+            boxes[i, 2] = (boxes[i, 4] * boxes[i, 2] + posTerms[i, 1] * posTerms[i, 4]) / (boxes[i, 4] + posTerms[i, 1])
+            boxes[i, 3] = (boxes[i, 4] * boxes[i, 3] + posTerms[i, 1] * posTerms[i, 5]) / (boxes[i, 4] + posTerms[i, 1])
+            if negTerms[i, 0] > 0.01:
+                boxes[i, 4] = boxes[i, 4] - betas[iter] * boxes[i, 4] * negTerms[i, 0]
+                suppressIdx = (int)(negTerms[i, 2])
+                suppressRecodMat[suppressIdx, i] = suppressRecodMat[suppressIdx, i] + 1
+
+    for i in range(N):
+        if boxes[i, 4] < threshold:
+            swap_array_val(boxes, i, N-1)
+            swap_array_val(negTerms, i, N-1)
+            swap_array_val(posTerms, i, N-1)
+            N = N - 1
+
+    keep = [i for i in range(N)]
+    return keep
+
 def soft_nms_39(np.ndarray[float, ndim=2] boxes, float sigma=0.5, float Nt=0.3, float threshold=0.001, unsigned int method=0):
     cdef unsigned int N = boxes.shape[0]
     cdef float iw, ih, box_area
